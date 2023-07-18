8e565b8fc38eaed32bc2e48f1eed2d71fc40bce0
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-07-17 20:57:14 -0700

    Add files via upload

---
+++ grpAnag.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        HashMap<String,List<String>> hmap = new HashMap<>();
+        for(String str: strs)
+        {
+            char stArr[] = str.toCharArray();
+            Arrays.sort(stArr);
+            String sort = String.valueOf(stArr);
+            if(!hmap.containsKey(sort))
+            {
+                hmap.put(sort,new ArrayList<>());
+            }
+            hmap.get(sort).add(str);
+        }
+        return new ArrayList<>(hmap.values());
+
+    }
+}
