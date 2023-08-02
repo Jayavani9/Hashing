6a35056739706f7ad8372640f510a1ef86a4112f
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-01 22:08:25 -0700

    Update WordPattern.java

--- WordPattern.java
+++ WordPattern.java
@@ -10,3 +10,30 @@ Output: true
 Example 2:
 Input: pattern = "abba", s = "dog cat cat fish"
 Output: false
+
+class Solution {
+  public boolean wordPattern(String pattern, String s) {
+        String words[] = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+        HashMap<Character, String> map = new HashMap<>();
+        for(int i = 0; i < pattern.length();i++)
+        {
+            char ch = pattern.charAt(i);
+            String w = words[i];
+            if(map.containsKey(ch))
+            {
+                if(!map.get(ch).equals(w)) return false;
+            }
+            else
+            {
+                if (map.containsValue(w)) { //Ex: pattern: ab and the string s: cat cat
+                    return false;
+                }
+                map.put(ch,w);
+            }
+        }
+        return true;
+    }
+}
