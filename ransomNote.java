b157754e9ab3f6008ba78797ada9aa6f448e7288
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-01 21:09:51 -0700

    Update ransomNote.java

--- ransomNote.java
+++ ransomNote.java
@@ -1,1 +1,47 @@
+383. Ransom Note
+  
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+Each letter in magazine can only be used once in ransomNote.
 
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+  
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+class Solution {
+    //Tc: O(m+n) and Sc: O(m)
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> hmap = new HashMap<>();
+        for(int i = 0 ; i < magazine.length();i++)
+        {
+            char ch = magazine.charAt(i);
+            hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
+           
+
+        }
+        for(int i = 0 ; i < ransomNote.length();i++)
+        {
+            char ch = ransomNote.charAt(i);
+            if(hmap.containsKey(ch))
+            {
+                int cnt = hmap.get(ch);
+                if(cnt > 0)
+                {
+                    hmap.put(ch,cnt-1);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+           return true;
+    }
+}
