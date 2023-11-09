c7d6c69023e547bbdf97fff0947df160ee55c8fb
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-11-08 19:51:30 -0800

    Update contiguosArr.java

--- contiguosArr.java
+++ contiguosArr.java
@@ -15,3 +15,26 @@ Output: 2
 Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
 
 
+//Bruteforce approach (TLE solution)
+//Tc: O(n^2) and Sc: O(1)
+  class Solution {
+    public int findMaxLength(int[] nums) {
+        int maxim = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            int zeros = 0;
+            int ones = 0;
+            for(int j = i ; j < nums.length; j++)
+            {
+                if(nums[j] == 0) zeros += 1;
+                else ones += 1;
+                if(zeros == ones)
+                {
+                    maxim = Math.max(maxim, j-i+1);
+                }   
+            }
+        }
+        return maxim;
+    }
+}
+
+
