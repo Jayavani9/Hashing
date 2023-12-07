5828c09af2c0a4ecff8456a10ca0701c629d380a
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-12-06 16:30:20 -0800

    Create FirstUniqueChar.java

---
+++ FirstUniqueChar.java
@@ -0,0 +1,48 @@
+387. First Unique Character in a String
+
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+class Solution {
+    public int firstUniqChar(String s) {
+      // Tc: O(n) and Sc: O(n)
+      HashMap<Character, Integer> map = new HashMap<>();
+      for(int i = 0; i < s.length(); i++)
+      {
+        char ch = s.charAt(i);
+        map.put(ch,map.getOrDefault(ch,0)+1);
+      }
+
+      for(int i = 0 ; i < s.length() ; i++)
+      {
+        if(map.get(s.charAt(i)) == 1) return i;
+      }
+      return -1;
+      /*
+        for (int i = 0; i < s.length(); i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < s.length(); j++) {
+                if (i != j && s.charAt(i) == s.charAt(j)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                return i;
+            }
+        }
+        return -1;
+        */
+    }
+}
