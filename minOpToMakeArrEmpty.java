220a8468d7da21b4f9753c55c0d338c44a2f6de6
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-07-16 13:35:51 -0700

    Update minOpToMakeArrEmpty.java

--- minOpToMakeArrEmpty.java
+++ minOpToMakeArrEmpty.java
@@ -1,39 +1,19 @@
 //https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/description/
 
 class Solution {
+    //Tc: O(n) Sc: O(n)
     public int minOperations(int[] nums) {
-         Tc: O(n) Sc: O(n)
-         int ans = 0;
+        int ans = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for (int val : map.values()) {
-            if (val == 1) {
-                return -1; 
-            }
-
-            int operations = 0;
-
-            operations += val / 3;
-            int remainder = val % 3;
-
-            if (remainder == 1) {
-             
-                if (val >= 4) {
-                    operations += 1; 
-                } else {
-                    return -1; 
-                }
-            } else if (remainder == 2) {
-                operations += 1; 
-            }
-
-            ans += operations;
+        for(int c: map.values())
+        {
+            if(c ==1) return -1;
+        ans += Math.ceil((double) c/3);
         }
-
         return ans;
-        
     }
 }
